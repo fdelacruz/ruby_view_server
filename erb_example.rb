b3f64efe53d76119a3161655a280e787355ed26d
Francisco De La Cruz <helloquico@gmail.com>
2013-10-26 13:56:35 -0400

    homework sections 1-3 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,12 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
-puts template.result(binding)
+arr = ["a", "b", "c"]
+hash = {coffee: "americano", tea: "chai"}
+
+template1 = ERB.new "The value of x is: <%= x %>"
+template2 = ERB.new "The value of arr is: <%= arr %>"
+template3 = ERB.new "The value of hash is: <%= hash %>"
+puts template1.result(binding)
+puts template2.result(binding)
+puts template3.result(binding)
